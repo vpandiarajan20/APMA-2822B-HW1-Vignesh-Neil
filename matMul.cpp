4fc27aa9b667f0fc0cfc3235904ac94d4c3ce8d2
Vignesh <vpandiarajan20@gmail.com>
2023-09-27 23:18:56 -0400

    labeling

--- matMul.cpp
+++ matMul.cpp
@@ -8,31 +8,16 @@ int main(int argc, char* argv[]) {
         std::cerr << "Usage: " << argv[0] << " <n> <m>" << std::endl;
         return 1;  // Return an error code indicating incorrect usage
   }
+  // Convert the command-line arguments to integers
+  int n = std::atoi(argv[1]);
+  int m = std::atoi(argv[2]);
 
-    // Convert the command-line arguments to integers
-    int num1 = std::atoi(argv[1]);
-    int num2 = std::atoi(argv[2]);
-
-  const int n = 5;
-  const int m = 5;
-
-
-  // int mat1[n][m];
-  // int mat2[m][n];
-  // int result[n][n];
-
-  // for (int i = 0; i < n; i++) {
-  //   for (int j = 0; j < n; j++) {
-  //       for (int k = 0; k < m; k++) {
-  //           result[i][j] += mat1[i][k] * mat2[k][j];
-  //       }
-  //   }
-  // }
 
   // double mat[n][m];
   // double vec[m];
   // double result[n];
 
+  // randomization
   // std::random_device rd;
   // std::mt19937 gen(rd());  // Mersenne Twister 19937 generator
   // std::uniform_real_distribution<double> distribution(1.0, 100.0);
@@ -48,6 +33,7 @@ int main(int argc, char* argv[]) {
 
   
 
+  // actual vector multiplication
   // for (int i = 0; i < n; i++) {
   //   for (int j = 0; j < m; j++) {
   //     result[i] += mat[i][m]*vec[m];
@@ -78,6 +64,7 @@ int main(int argc, char* argv[]) {
   // double **mat;
   // mat = new double*[n];
   // mat[0] = new double[n*m];
+
   // double *vec;
   // vec = new double[m];
 
